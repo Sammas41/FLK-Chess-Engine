1c3b194b01f5b375608b43bed85764d21cc3a9e3
RichardNixon <nicola.schiavo@studenti.unipd.it>
2024-02-27 15:48:03 +0100

    add pieces moves

--- src/moveGenerator.h
+++ src/moveGenerator.h
@@ -10,6 +10,19 @@ class MoveGenerator
     Game game;
     std::unique_ptr<Attacks> ptr_attacks;
 
+    private:
+
+        void generate_white_pawns_moves(int ,U64&);
+        void generate_white_king_castling_moves(int , U64&);
+        void generate_black_pawns_moves(int ,U64&);
+        void generate_black_king_castling_moves(int , U64&);
+        void generate_knights_moves(int, U64&, int);
+        void generate_bishops_moves(int, U64&, int);
+        void generate_rooks_moves(int, U64&, int);
+        void generate_queens_moves(int, U64&, int);
+        void generate_kings_moves(int, U64&, int);
+
+
     public:
         MoveGenerator() = delete;   // Cannot initialize MoveGenerator without a game
         MoveGenerator(Game &);      // Default constructor
@@ -18,6 +31,7 @@ class MoveGenerator
         void print_attacked_squares(int);
 
         void generate_moves();
+
 };
 
 #endif
