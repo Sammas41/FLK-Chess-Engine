97f006e0484ae7f0359954069cb97fac55bea096
RichardNixon <nicola.schiavo@studenti.unipd.it>
2024-09-30 21:38:42 +0200

    add take back functionality

--- src/engine.h
+++ src/engine.h
@@ -5,18 +5,28 @@
 #include "moveGenerator.h"
 #include "search.h"
 
+struct InputResult {
+    bool is_move;
+    Move move;
+};
 class Engine {
     public:
         Engine();
         Engine(std::string);
 
         void play(char);
+        void takeback();
         bool is_mate(Game&);
+        std::string take_input();
+        InputResult process_input(int colour, std::vector<Game>& game_history);
+        Move create_move(std::string input, int colour);
+        bool is_legal_move(Move& move);
+        void undo(int steps, std::vector<Game>& game_history);
     private:
         Game game;
 
         bool is_running();
-        Move get_player_move(int);
+        Move get_player_move(int, std::string);
         Move search_position(int);
 };
 
